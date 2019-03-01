e5428342bf4c33071bdf055abbb2c89c651780b2
Daniel Munday <mnddan004@myuct.ac.za>
2019-03-01 17:50:31 +0200

    Base framework for client-server communication added

--- src/Server/server.java
+++ src/Server/server.java
@@ -1,8 +1,64 @@
 package Server;
-/**
- *
- * @author Daniel
- */
+import java.net;
+import java.io.IOException;
+
+// defines the server in client-server communication
 public class server {
-    
+    ServerSocket serverSocket; // MyService
+    Socket clientSocket;
+    DataInputStream input;
+    PrintStream output;
+    // constructor
+    public server(int port){
+        /**
+        * sets up the server socket to listen to and accept
+        * connection requests given a port number
+        */
+        try{
+            ss = new ServerSocket(port);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    // creates a client socket to facilitate communication with 
+    // a specific client
+    public void initiateConnection(int port){ 
+        clientSocket = null;
+        try{
+            clientSocket = ss.accept();
+        } catch (IOException e){
+            System.out.println(e);
+        }   
+    }
+
+    // gets message data from client socket
+    public String getInput(){
+        try{
+            input = new DataInputStream(ss.getInputStream());
+        } catch (IOException e){
+            System.out.println(e);
+        }
+    }
+
+    // sends a message to the client
+    public void outputMessage(){
+        try{
+            output = new PrintStream(ss.getOutputStream());
+        } catch (IOException e){
+            System.out.println(e);
+        }
+    }
+
+    // closes all sockets and data streams
+    public void close(){
+        try{
+            output.close();
+            input.close();
+            ss.close();
+            clientSocket.close();
+        } catch (IOException e){
+            System.out.println(e);
+        }
+    }
 }
