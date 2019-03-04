64e71ea1fe1d25b6587d71d7d5db4535247b4743
KuntaNgcobo <44111959+KuntaNgcobo@users.noreply.github.com>
2019-03-04 04:29:13 +0200

    Added functionality
    Client can add messages to the chat

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,51 +1,63 @@
 package Client;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 // defines a client in client-server communication
-public class client {
-    Socket clientSocket;
-    DataInputStream input;
-    PrintStream output;
+public class Client extends Thread{
+    String hostName, message;
+    Scanner input;
+    PrintWriter output;
+    int port;
+    
     // constructor
+    public Client(){
+
+    }
     // initialises with client's name and port number
-    public client(String name, int port){
-        try{
-            clientSocket = new Socket(name, port);
-        } catch (IOException e){
-            System.out.println(e);
-        }
+    public Client(String msg, String name, int port){
+        this.message = msg;
+        this.hostName = name;
+        this.port = port;
+    }
+    
+    public String getHostName(){
+        return this.hostName;
+    }
+    
+    public int getPort(){
+        return this.port;
     }
 
-    // receives a message
-    public void receiveMsg(){
-        try{
-            input = new DataInputStream(clientSocket.getInputStream());
-        } catch (IOException e){
+    @Override
+    public void run(){
+        try {
+            Socket s = new Socket(hostName, port);
+            input = new Scanner(s.getInputStream());
+            output = new PrintWriter(s.getOutputStream(), true);
+            s.getOutputStream().write(message.getBytes());
+            while(input.hasNextLine()){
+                output.write(input.nextLine() + "\n");
+            }
+            s.close();
+        } catch (IOException e) {
             System.out.println(e);
+        } finally {
+            //s.close();
         }
     }
-
-    // outputs a message
-    public void sendMsg(){
-        try{
-            output = new PrintStream(clientSocket.getOutputStream());
-        } catch (IOException e){
-            System.out.println(e);
-        }
-    }
-
-    // closes connection and input/output streams
-    public void close(){
-        try{
-            input.close();
-            output.close();
-            clientSocket.close();
-        } catch (IOException e){
-            System.out.println(e);
+    
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.err.println("Pass the server IP as the sole command line argument");
+            return;
         }
     }
 }
